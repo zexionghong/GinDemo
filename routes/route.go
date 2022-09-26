336a50b085f169ec392dda3602c07ea47cf5ea75
hzx <hzx@>
2022-09-26 10:23:17 +0800

    提交

--- routes/route.go
+++ routes/route.go
@@ -5,7 +5,21 @@ import (
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "ipData/docs"
+	"net/http"
 )
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} ok
+// @Router /health [get]
+func CheckHealth(g *gin.Context) {
+	g.JSON(http.StatusOK, "ok")
+}
 
 func InitRouter() *gin.Engine {
 	Router := gin.Default()
@@ -14,9 +28,7 @@ func InitRouter() *gin.Engine {
 	PublicGroup := Router.Group("")
 	{
 		// 健康监测
-		PublicGroup.GET("/health", func(c *gin.Context) {
-			c.JSON(200, "ok")
-		})
+		PublicGroup.GET("/health", CheckHealth)
 	}
 	return Router
 }
